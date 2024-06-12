c416427603742a60f54328d19ffa4a1549e81a95
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-06-12 14:12:47 +0630

    Update config json file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+'use client';
 import React from 'react';
 import ClientLayout from './ClientLayout';
 import Search from './component/Search';
+
 const Page = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClientLayout>
-       <Search />
+      <Search />
       {children}
     </ClientLayout>
   );
